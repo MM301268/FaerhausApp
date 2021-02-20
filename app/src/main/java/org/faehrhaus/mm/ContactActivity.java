3d740bb79d6a6bbd15909a06a0bf7f0b06fd993d
MM301268 <Markus.Meier68@gmx.de>
2021-02-20 23:00:00 +0100

    StepsActivity implemented

--- app/src/main/java/org/faehrhaus/mm/ContactActivity.java
+++ app/src/main/java/org/faehrhaus/mm/ContactActivity.java
@@ -1,6 +1,9 @@
 package org.faehrhaus.mm;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +12,14 @@ public class ContactActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+
+        Button btn_CA_Next = findViewById(R.id.btn_CA_next);
+        btn_CA_Next.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(ContactActivity.this, StepsActivity.class));
+            }
+        });
     }
 }
