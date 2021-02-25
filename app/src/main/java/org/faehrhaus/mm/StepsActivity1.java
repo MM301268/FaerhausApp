eebe39d379174cc856580807e04fbc13f4237b27
MM301268 <Markus.Meier68@gmx.de>
2021-02-25 19:43:54 +0100

    StepsActivity2 implemented

--- app/src/main/java/org/faehrhaus/mm/StepsActivity1.java
+++ app/src/main/java/org/faehrhaus/mm/StepsActivity1.java
@@ -6,19 +6,18 @@ import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ContactActivity extends AppCompatActivity {
+public class StepsActivity1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contact);
+        setContentView(R.layout.activity_steps1);
         getSupportActionBar().hide();
-        Button btn_CP_Next = findViewById(R.id.btn_CP_next);
-        btn_CP_Next.setOnClickListener(new View.OnClickListener()
-        {
+        Button btn_SP1_Next = findViewById(R.id.btn_SP1_Next);
+        btn_SP1_Next.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                startActivity(new Intent(ContactActivity.this, StepsActivity1.class));
+            public void onClick(View v) {
+                startActivity(new Intent(StepsActivity1.this, StepsActivity2.class));
             }
         });
     }
--- app/src/main/java/org/faehrhaus/mm/StepsActivity2.java
+++ app/src/main/java/org/faehrhaus/mm/StepsActivity2.java
@@ -2,14 +2,18 @@ package org.faehrhaus.mm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class StepsActivity extends AppCompatActivity {
+public class StepsActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_steps);
+        setContentView(R.layout.activity_steps2);
         getSupportActionBar().hide();
+
     }
 }
